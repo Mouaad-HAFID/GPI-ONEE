48c8b092a7fb331d887e8d7750608d45c0329f26
Equinox <mouaad.mh.hafid@gmail.com>
2021-07-26 15:41:14 +0100

    FrontEnd Changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (res) => {
+        this.users = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
