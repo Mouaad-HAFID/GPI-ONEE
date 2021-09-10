2ee6d0e3b70bb4f47bc1bbf6cdcd366f7e7da779
Equinox <mouaad.mh.hafid@gmail.com>
2021-09-10 06:23:04 +0100

    Model Modification

--- client/src/app/_models/typeEquipement.ts
+++ client/src/app/_models/typeEquipement.ts
@@ -1,7 +1,9 @@
+import { Equipement } from './equipement';
 import { Gamme } from './gamme';
 
 export interface TypeEquipement {
   id: number;
   nom: string;
   gammes: Gamme[];
+  equipement: Equipement[];
 }
